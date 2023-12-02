55392732677361f61fa1217802c93d199dbae0fd
Sara <148695739+sarickaaa123@users.noreply.github.com>
2023-12-02 22:37:38 +0100

    Update Soberi.java

--- src/project_21/Soberi.java
+++ src/project_21/Soberi.java
@@ -3,8 +3,20 @@ package project_21;
 public class Soberi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		package projcet_21;
+import java.util.Scanner;
+public class Clenovi{
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.print("Clenovi koi se delivi so 4");
+	for (int i=1 && i<=n &&i++);
+	if(i%4==0 && i%7==0);
+			System.out.print("it");
+		
+	
+ }
 }
+
+
+
+
